49909c5797dc05c734fe6fa927beb67249342c97
Alex Smith <alex@vxpro.com>
2021-05-12 21:51:32 -0700

    fix rename script to ignore .git folder

--- rename_project.sh
+++ rename_project.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+myname="vx-cmake-template"
 curdirr=$(pwd)
 dirr=$(basename $curdirr)
-if [ $dirr == "vx-cmake-template" ] ; then
+if [ $dirr == "$myname" ] ; then
     echo Run this script inside a new folder to rename
-    echo vx-cmake-template to a current \'useful\' name
+    echo $myname to a current \'useful\' name
     exit 1
 fi
 
 [ -d build ] && rm -rf build
 [ -d .vscode/ipch ] && rm -rf .vscode/ipch
 
-echo Renaming \"vx-cmake-template\" to $dirr
+echo Renaming \"$myname\" to $dirr
 
 while read filer
 do 
-    sed -i "s|vx-cmake-template|$dirr|g" $filer
+    if [[ $filer =~ ^\./\.git ]] ; then
+            continue
+    fi
+    sed -i "s|$myname|$dirr|g" $filer
     if [[ $filer =~ ^.*vx-cmake-template.*$ ]] ; then
-        newfiler=${filer/vx-cmake-template/$dirr}
+        newfiler=${filer/$myname/$dirr}
         mv $filer $newfiler
     fi
 done < <(find . -type f)
